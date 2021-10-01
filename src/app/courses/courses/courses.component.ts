e5cbedc4e502393408b34c28d2833804fcc6daba
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2021-10-01 01:17:38 -0300

    create table of courses

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    { _id: '0', name: 'Angular', category: 'Front-End' }
+  ];
+  displayedColumns = ['name', 'category'];
 
   constructor() { }
 
