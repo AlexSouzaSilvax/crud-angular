ba3d672ba93b4bc9bba55ec2484d48ffdaf571f8
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2022-05-30 21:49:51 -0300

    aula 10

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,21 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-
-  courses: Course[] = [
-    { _id: '0', name: 'Angular', category: 'Front-End' }
-  ];
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private coursesService: CoursesService) {
+    this.courses$ = this.coursesService.list();
   }
 
+  ngOnInit(): void {}
 }
